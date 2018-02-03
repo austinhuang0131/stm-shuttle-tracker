486a5906186348a5f0a54f1ccbbbb96517747e98
Glitch (exuberant-authority) <none>
2018-02-03 17:29:48 +0000

    🔥🙇 Checkpoint server.js:529300/328 database.json:529300/3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs")
-var db = JSON.parse(fs.readFileSync(""));
+var db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,18 +13,10 @@ var db = JSON.parse(fs.readFileSync(""));
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
+app.post("/database", function (request, response) {
+  db.push(request.query);
+  fs.writeFile("./database.json", JSON.stringify(db), "utf8");
   response.sendStatus(200);
 });
 
