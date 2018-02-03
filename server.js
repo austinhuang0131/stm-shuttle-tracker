fd75c6368ef8941e18da61526a728a2a540ebcdb
Glitch (exuberant-authority) <none>
2018-02-03 17:18:56 +0000

    🎈🚤 Checkpoint server.js:529300/88 database.json:529300/1

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+var fs = require("fs")
+var db = JSON.parse(fs.readFileSync(""));
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -25,13 +27,6 @@ app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream);
   response.sendStatus(200);
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
