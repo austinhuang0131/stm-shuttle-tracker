4397398fab956d8a6efc88b750cb288d61c78dce
Glitch (exuberant-authority) <none>
2018-02-03 17:51:31 +0000

    👵👦 Checkpoint server.js:529300/36

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.post("/database", function (request, response) {
   fs.writeFile("./database.json", JSON.stringify(db), "utf8");
   response.sendStatus(200);
 });
+app.get("/database", function (request, response) {
+  response.send(db.find(a => {return a.user === request.query.user}));
+});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
