e0ee220c5102e68aea04f027c3d0033d9fa21434
Glitch (exuberant-authority) <none>
2018-02-04 03:37:43 +0000

    🔑🍤 Checkpoint server.js:529300/200

--- server.js
+++ server.js
@@ -11,11 +11,17 @@ app.use(express.static('public'));
 
 app.post("/database", function (request, response) {
   db.push(request.query);
+  if (db.find(a => {return a.user === request.query.user})) db.slice(db.indexOf(db.find(a => {return a.user === request.query.user})), 0);
   fs.writeFile("./database.json", JSON.stringify(db), "utf8");
   response.sendStatus(200);
 });
 app.get("/database", function (request, response) {
-  response.send(db.find(a => {return a.user === request.query.user}));
+  if (db.find(a => {return a.user === request.query.user}) === undefined) {
+    var o = {user: request.query.user, goal: 60};
+    db.push(o);
+    response.send(o);
+  }
+  else response.send(db.find(a => {return a.user === request.query.user}));
 });
 
 var listener = app.listen(process.env.PORT, function () {
