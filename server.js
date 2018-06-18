20271946d71ff5643c2a2620e1440f780a8723f1
Glitch (exuberant-authority) <none>
2018-06-18 21:54:45 +0000

    📰⏰ Checkpoint ./package.json:529300/1486 ./server.js:529300/431

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ var request = require("request");
 var cheerio = require("cheerio");
 var snekfetch = require("snekfetch");
 var moment = require("moment");
+var bodyParser = require('body-parser');
 
 require("moment-duration-format")(moment);
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 /*  snekfetch.get("https://www.fanfiction.net/s/12954493").then(r => {
@@ -26,5 +29,11 @@ app.get("/", (req, res) => {
   var now = moment();
   res.send(moment.duration(Date.now()).format("d [days] hh [hours] mm [minutes]"));
   */
+  res.set('Content-Type', 'application/json');
   request("https://discoin.sidetrip.xyz/transactions", {headers: {"Authorization": req.get("Authorization")}}, (err, resn, body) => res.send(body))
 })
+
+app.post("/transaction", (req, res) => {
+  res.set('Content-Type', 'application/json');
+  request.post("https://discoin.sidetrip.xyz/transactions", {headers: {"Authorization": req.get("Authorization")}, body: req.body}, (err, resn, body) => res.send(body))
+});
