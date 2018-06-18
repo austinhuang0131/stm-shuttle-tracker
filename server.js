609ffc9f7a40c02119dc1cc5fdfe804f57eb3781
Glitch (exuberant-authority) <none>
2018-06-18 21:20:48 +0000

    💡🚥 Checkpoint ./server.js:529300/1008 ./package.json:529300/2890

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var request = require("request");
 var cheerio = require("cheerio");
 var snekfetch = require("snekfetch");
 var moment = require("moment");
@@ -21,7 +22,12 @@ app.get("/", (req, res) => {
     var chapters = $("#chap_select option");
     res.send((chapters.length / 2).toString());
   });
-*/
+
   var now = moment();
   res.send(moment.duration(Date.now()).format("d [days] hh [hours] mm [minutes]"));
+  */
+  snekfetch.get("https://discoin.sidetrip.xyz/transactions", {headers: {Authorization: "80ab72d40aef21028dbf4e68326b6e1cecceeca37347a9e84eda1a8386fbe463"}}).then(r =>
+    res.send(r.body)
+  ).catch(e => res.send(e));
+  /*request("https://discoin.sidetrip.xyz/transactions", {headers: {"Authorization": "80ab72d40aef21028dbf4e68326b6e1cecceeca37347a9e84eda1a8386fbe463"}}, (err, resn, body) => res.send(body))*/
 })
