5dd4ed3c0c18271f114e303c883ad0670b63e4ec
Glitch (exuberant-authority) <none>
2018-07-29 16:34:45 +0000

    🍠🛢 Checkpoint ./package.json:529300/1548 ./server.js:529300/666

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var express = require('express');
 express.json();
 var app = express();
 var fs = require("fs");
-var snekfetch = require("snekfetch");
-var request = require("request");
+var Pixiv = require("pixiv-app-api");
+var pixiv = new Pixiv("austinhuang0131@icloud.com", "metagon123");
 var bodyParser = require('body-parser');
 
 var listener = app.listen(process.env.PORT, function () {
@@ -17,12 +17,5 @@ var listener = app.listen(process.env.PORT, function () {
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  request.post("https://oauth.secure.pixiv.net/auth/token", {form: {
-    "grant_type":"password",
-    "username":"austinhuang0131@icloud.com",
-    "password":"metagon123",
-    "get_secure_url":"1",
-    "client_id":"KzEZED7aC0vird8jWyHM38mXjNTY",
-    "client_secret":"W9JZoJe00qPvJsiyCGT3CCtC6ZUtdpKpzMbNlUGP"
-  }}, (e, r, b) => {console.log(b)})
+  pixiv.searchIllust("succubus").then(r => res.send(r));
 })
