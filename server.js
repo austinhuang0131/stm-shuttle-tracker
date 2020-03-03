87accc9edcfc9cce3596becb9f1c607d86b8037f
Glitch (exuberant-authority) <none>
2020-03-03 18:23:14 +0000

    🍵🙎 Checkpoint ./server.js:529300/275 ./package.json:529300/3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var request = require('request');
 express.json();
 var app = express();
 var bodyParser = require('body-parser');
-var GtfsRealtimeBindings = require('gtfs-realtime-bindings').transit_realtime;
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
@@ -15,9 +15,8 @@ var listener = app.listen(process.env.PORT, function () {
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  request("https://api.stm.info/pub/od/gtfs-rt/ic/v1/tripUpdates", {method: "POST", headers: {apikey: "l7xx37a10aa967e44c2690c564d094e6abc7", encoding: null}}, (e,r,b) => {
-    res.send(b)
-    let feed = GtfsRealtimeBindings.FeedMessage.decode(Buffer.from(b));
-    console.log(feed.entity)
+  request("https://api.stm.info/pub/od/gtfs-rt/ic/v1/tripUpdates", {method: "POST", headers: {apikey: "l7xx37a10aa967e44c2690c564d094e6abc7"}, encoding: null}, (e,r,b) => {
+    let feed = GtfsRealtimeBindings.FeedMessage.decode(b);
+    res.send(feed)
   })
 })
