8f6220c3fd18e9062964885485a47c243a13ac4d
Glitch (exuberant-authority) <none>
2020-03-03 19:21:32 +0000

    🌆📕 Checkpoint ./server.js:529300/130

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var listener = app.listen(process.env.PORT, function () {
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/:route", (req, res) => {
   request("https://api.stm.info/pub/od/gtfs-rt/ic/v1/vehiclePositions", {method: "POST", headers: {apikey: "l7xx37a10aa967e44c2690c564d094e6abc7"}, encoding: null}, (e,r,b) => {
     let feed = GtfsRealtimeBindings.FeedMessage.decode(b);
-    res.send(feed.entity.filter(f => f.vehicle.trip.route_id === "12").map(r => "Bus number " + r.id + " is currently "+(r.vehicle.current_status === 2 ? "going to" : "at")+" stop no. " + r.vehicle.current_stop_sequence + " ("+r.vehicle.position.latitude + ", " + r.vehicle.position.longitude + ") with trip #"+r.vehicle.trip.id).join("<br>")+"<br><br>"+JSON.stringify(feed.entity.filter(f => f.vehicle.trip.route_id === "12")));
+    res.send(feed.entity.filter(f => f.vehicle.trip.route_id === req.params.route).map(r => "Bus number " + r.id + " is currently "+(r.vehicle.current_status === 2 ? "going to" : "at")+" stop no. " + r.vehicle.current_stop_sequence + " ("+r.vehicle.position.latitude + ", " + r.vehicle.position.longitude + ") with trip #"+r.vehicle.trip.trip_id).join("<br><br>"));
   })
 })
