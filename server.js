7c04f8a128e6eb1785ef5f742daa2bacf626b389
Glitch (exuberant-authority) <none>
2020-03-09 14:13:26 +0000

    🛰🕰 Checkpoint ./server.js:529300/1378

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var request = require("request");
+const express = require("express"),
+      request = require("request"),
+      GtfsRealtimeBindings = require("gtfs-realtime-bindings"),
+      fs = require("fs");
 var app = express();
-var GtfsRealtimeBindings = require("gtfs-realtime-bindings");
 var listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
@@ -17,33 +18,11 @@ app.get("/:route", (req, res) => {
     (e, r, b) => {
       let feed = GtfsRealtimeBindings.FeedMessage.decode(b);
       res.send(
-        feed.entity
-          .filter(f => f.vehicle.trip.route_id === req.params.route)
-          .length === 0
-        ? "There are no buses on this route :("
-        : feed.entity
-          .filter(f => f.vehicle.trip.route_id === req.params.route)
-          .map(
-            r =>
-              "Bus number " +
-              r.id +
-              " is currently " +
-              (r.vehicle.current_status === 2 ? "going to" : "at") +
-              " stop no. " +
-              r.vehicle.current_stop_sequence +
-              " (" +
-              r.vehicle.position.latitude +
-              ", " +
-              r.vehicle.position.longitude +
-              ") with trip #" +
-              r.vehicle.trip.trip_id
-          )
-          .join("<br><br>")
       );
     }
   );
 });
 
 app.get("/", (req, res) => {
-  res.send("<p>The rule is very simple: All you need to do is visit https://maribus-test.glitch.me followed by the route number.</p><p>For example, if you want to track that shuttle bus, you would do https://maribus-test.glitch.me/124E. (Don't forget the E! Otherwise you'd be tracking the regular 124.)</p>Of course, currently, everything is still in the bare minimum stage and I haven't implemented most of the features yet, so bear with me...")
+  res.send("")
 })
