1145544bd3f53a0bb6fd287f589badbc1b376c6b
Glitch (stmbus) <none>
2020-03-11 00:19:19 +0000

    👱🚖 Checkpoint ./server.js:529300/17567 ./watch.json:529300/4

--- server.js
+++ server.js
@@ -58,160 +58,167 @@ app.get("/changelog", (req, res) => {
 
 app.get("/:school", (req, res) => {
   if (
-    (time === "EDT" &&
-      new Date().getUTCHours() < 11 &&
-      new Date().getUTCHours() > 23) ||
-    (time === "EST" && new Date().getUTCHours() < 12)
-  ) res.status(503).sendFile(__dirname + "/unavailable.html");
+    (time === "EDT" && new Date().getUTCHours() < 11) ||
+    (time === "EST" &&
+      new Date().getUTCHours() < 12 &&
+      new Date().getUTCHours() !== 0)
+  )
+    res.status(503).sendFile(__dirname + "/unavailable.html");
   else if (!list[req.params.school]) res.status(404).send("Invalid school.");
-  else routes.fetch(req.params.school).then(async x => {
-    let t = await DB.fetch("time." + req.params.school),
-      old = await DB.fetch("old." + req.params.school);
-    if (!x) res.status(501).send("No bus data yet. This could be due to the school being relatively new to the system. Wait a couple of hours.");
-    else
-      res.send(
-        sample
-          .replace(
-            "[BUSES]",
-            x
-              .map(r => {
-                if (list[req.params.school][r.vehicle.trip.tripId])
-                  return (
-                    "L.marker([" +
-                    r.vehicle.position.latitude +
-                    ", " +
-                    r.vehicle.position.longitude +
-                    '], {icon: greenIcon}).addTo(mymap).bindPopup("<p>Bus #' +
-                    r.id +
-                    ", bound for " +
-                    (list[req.params.school][r.vehicle.trip.tripId].up
-                      ? routelist[req.params.school].up
-                      : routelist[req.params.school].down) +
-                    ", which departs at <b>" +
-                    list[req.params.school][r.vehicle.trip.tripId].time +
-                    "</b>, is " +
-                    (r.vehicle.currentStatus === 1
-                      ? "stopping at <b>"
-                      : "going to <b>") +
-                    routelist[req.params.school].stops[
+  else
+    routes.fetch(req.params.school).then(async x => {
+      let t = await DB.fetch("time." + req.params.school),
+        old = await DB.fetch("old." + req.params.school);
+      if (!x)
+        res
+          .status(501)
+          .send(
+            "No bus data yet. This could be due to the school being relatively new to the system. Wait a couple of hours."
+          );
+      else
+        res.send(
+          sample
+            .replace(
+              "[BUSES]",
+              x
+                .map(r => {
+                  if (list[req.params.school][r.vehicle.trip.tripId])
+                    return (
+                      "L.marker([" +
+                      r.vehicle.position.latitude +
+                      ", " +
+                      r.vehicle.position.longitude +
+                      '], {icon: greenIcon}).addTo(mymap).bindPopup("<p>Bus #' +
+                      r.id +
+                      ", bound for " +
                       (list[req.params.school][r.vehicle.trip.tripId].up
-                        ? "u"
-                        : "d") + r.vehicle.currentStopSequence
-                    ] +
-                    "</b> (" +
-                    r.vehicle.currentStopSequence +
-                    ") at " +
-                    new Date(r.vehicle.timestamp * 1000)
-                      .toLocaleString("en-US", {
-                        timeZone: "America/Montreal",
-                        hour12: false
-                      })
-                      .split(" ")[1] +
-                    (routelist[req.params.school].uptime &&
-                    routelist[req.params.school].downtime
-                      ? ".</p><p>The bus is predicted to arrive at " +
+                        ? routelist[req.params.school].up
+                        : routelist[req.params.school].down) +
+                      ", which departs at <b>" +
+                      list[req.params.school][r.vehicle.trip.tripId].time +
+                      "</b>, is " +
+                      (r.vehicle.currentStatus === 1
+                        ? "stopping at <b>"
+                        : "going to <b>") +
+                      routelist[req.params.school].stops[
                         (list[req.params.school][r.vehicle.trip.tripId].up
-                          ? routelist[req.params.school].up
-                          : routelist[req.params.school].down) +
-                        " in " +
-                        humanizeDuration(
-                          new Date(
-                            new Date()
-                              .toLocaleString("en-US", {
-                                timeZone: "America/Montreal"
-                              })
-                              .split(",")[0] +
-                              " " +
-                              list[req.params.school][r.vehicle.trip.tripId]
-                                .time +
-                              " " +
-                              time
-                          ).getTime() -
-                            Date.now() +
-                            (list[req.params.school][r.vehicle.trip.tripId].up
-                              ? routelist[req.params.school].uptime
-                              : routelist[req.params.school].downtime),
-                          { verbose: true, unitCount: 1 }
-                        ) +
-                        ", and come back to " +
-                        (list[req.params.school][r.vehicle.trip.tripId].up
-                          ? routelist[req.params.school].down
-                          : routelist[req.params.school].up) +
-                        " in " +
-                        humanizeDuration(
-                          new Date(
-                            new Date()
-                              .toLocaleString("en-US", {
-                                timeZone: "America/Montreal"
-                              })
-                              .split(",")[0] +
-                              " " +
-                              list[req.params.school][r.vehicle.trip.tripId]
-                                .time +
-                              " " +
-                              time
-                          ).getTime() -
-                            Date.now() +
-                            routelist[req.params.school].uptime +
-                            routelist[req.params.school].downtime,
-                          { verbose: true, unitCount: 1 }
-                        ) +
-                        '.</p>");'
-                      : '.</p>");')
-                  );
-                else
-                  return (
-                    "L.marker([" +
-                    r.vehicle.position.latitude +
-                    ", " +
-                    r.vehicle.position.longitude +
-                    '], {icon: greenIcon}).addTo(mymap).bindPopup("Bus ' +
-                    r.id +
-                    " is currently " +
-                    (r.vehicle.currentStatus === 1 ? "at" : "going to") +
-                    " stop no. " +
-                    r.vehicle.currentStopSequence +
-                    " with trip #" +
-                    r.vehicle.trip.tripId +
-                    '");'
-                  );
-              })
-              .join("\n")
-          )
-          .replace(
-            "[TIME]",
-            "<br><i>List updated at " +
-              new Date(t).toLocaleString("en-US", {
-                timeZone: "America/Montreal"
-              }) +
-              ".</i>"
-          )
-          .replace(
-            /\[SCHOOL\]/g,
-            routelist[req.params.school].name +
-              ", route " +
-              routelist[req.params.school].route
-          )
-          .replace("[school]", req.params.school)
-          .replace("[CENTER]", routelist[req.params.school].center)
-          .replace(
-            "[OLD]",
-            old === "yes"
-              ? '<p id="open">There are no buses running currently. This could mean that all the buses are being "En Transit", or a driver forgot to turn on iBUS... Below are the data acquired ' +
-                  humanizeDuration(Date.now() - t, {
-                    verbose: true,
-                    unitCount: 1,
-                    separateMilliseconds: true
-                  }) +
-                  " ago:</p>"
-              : '<p id="open">Below are the data acquired ' +
-                  humanizeDuration(Date.now() - t, {
-                    verbose: true,
-                    unitCount: 1,
-                    separateMilliseconds: true
-                  }) +
-                  " ago:</p>"
-          )
-      );
-  });
+                          ? "u"
+                          : "d") + r.vehicle.currentStopSequence
+                      ] +
+                      "</b> (" +
+                      r.vehicle.currentStopSequence +
+                      ") at " +
+                      new Date(r.vehicle.timestamp * 1000)
+                        .toLocaleString("en-US", {
+                          timeZone: "America/Montreal",
+                          hour12: false
+                        })
+                        .split(" ")[1] +
+                      (routelist[req.params.school].uptime &&
+                      routelist[req.params.school].downtime
+                        ? ".</p><p>The bus is predicted to arrive at " +
+                          (list[req.params.school][r.vehicle.trip.tripId].up
+                            ? routelist[req.params.school].up
+                            : routelist[req.params.school].down) +
+                          " in " +
+                          humanizeDuration(
+                            new Date(
+                              new Date()
+                                .toLocaleString("en-US", {
+                                  timeZone: "America/Montreal"
+                                })
+                                .split(",")[0] +
+                                " " +
+                                list[req.params.school][r.vehicle.trip.tripId]
+                                  .time +
+                                " " +
+                                time
+                            ).getTime() -
+                              Date.now() +
+                              (list[req.params.school][r.vehicle.trip.tripId].up
+                                ? routelist[req.params.school].uptime
+                                : routelist[req.params.school].downtime),
+                            { verbose: true, unitCount: 1 }
+                          ) +
+                          ", and come back to " +
+                          (list[req.params.school][r.vehicle.trip.tripId].up
+                            ? routelist[req.params.school].down
+                            : routelist[req.params.school].up) +
+                          " in " +
+                          humanizeDuration(
+                            new Date(
+                              new Date()
+                                .toLocaleString("en-US", {
+                                  timeZone: "America/Montreal"
+                                })
+                                .split(",")[0] +
+                                " " +
+                                list[req.params.school][r.vehicle.trip.tripId]
+                                  .time +
+                                " " +
+                                time
+                            ).getTime() -
+                              Date.now() +
+                              routelist[req.params.school].uptime +
+                              routelist[req.params.school].downtime,
+                            { verbose: true, unitCount: 1 }
+                          ) +
+                          '.</p>");'
+                        : '.</p>");')
+                    );
+                  else
+                    return (
+                      "L.marker([" +
+                      r.vehicle.position.latitude +
+                      ", " +
+                      r.vehicle.position.longitude +
+                      '], {icon: greenIcon}).addTo(mymap).bindPopup("Bus ' +
+                      r.id +
+                      " is currently " +
+                      (r.vehicle.currentStatus === 1 ? "at" : "going to") +
+                      " stop no. " +
+                      r.vehicle.currentStopSequence +
+                      " with trip #" +
+                      r.vehicle.trip.tripId +
+                      '");'
+                    );
+                })
+                .join("\n")
+            )
+            .replace(
+              "[TIME]",
+              "<br><i>List updated at " +
+                new Date(t).toLocaleString("en-US", {
+                  timeZone: "America/Montreal"
+                }) +
+                ".</i>"
+            )
+            .replace(
+              /\[SCHOOL\]/g,
+              routelist[req.params.school].name +
+                ", route " +
+                routelist[req.params.school].route
+            )
+            .replace("[school]", req.params.school)
+            .replace("[CENTER]", routelist[req.params.school].center)
+            .replace(
+              "[OLD]",
+              old === "yes"
+                ? '<p id="open">There are no buses running currently. This could mean that all the buses are being "En Transit", or a driver forgot to turn on iBUS... Below are the data acquired ' +
+                    humanizeDuration(Date.now() - t, {
+                      verbose: true,
+                      unitCount: 1,
+                      separateMilliseconds: true
+                    }) +
+                    " ago:</p>"
+                : '<p id="open">Below are the data acquired ' +
+                    humanizeDuration(Date.now() - t, {
+                      verbose: true,
+                      unitCount: 1,
+                      separateMilliseconds: true
+                    }) +
+                    " ago:</p>"
+            )
+        );
+    });
 });
